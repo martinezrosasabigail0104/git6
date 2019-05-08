50b9160ccb9a47256ed4001db38bc1e3f991b66d
AlbertGut <38224885+AlbertGut@users.noreply.github.com>
2019-05-08 08:48:20 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,13 @@
 //JavaScript Document
 
-$(document).ready(function(e) {
-    document.addEventListener("deviceready",function(){
+$(document).ready(function (e){
+    document.addEventListener ("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
 			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
 		});
 
-$('#derecha')on("swipeleft",function(){
+$('#derecha').on("swipeleft",function(){
 	navigator .notification. confirm ("¿Que quieres hacer?",function(opt){
 		switch(opt)
 		{
